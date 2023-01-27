9cff838da8e6ade66b6b3ad73a23e51fa43fff68
tmouton1 <tamia.mouton1@gmail.com>
2023-01-27 14:22:52 -0500

    List of Cupcakes added

--- Unit_2_Folder/server.py
+++ Unit_2_Folder/server.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, url_for, redirect
-from cupcakes import get_cupcakes, find_cupcake, add_cupcake_dictionary, cupcake_list
+from cupcakes import get_cupcakes, find_cupcake, add_cupcake_dictionary, cupcake1,cupcake2,cupcake3,cupcake4
 
 
 app = Flask(__name__)
@@ -24,12 +24,21 @@ def about_info():
     return render_template("info.html")
 
 # ===============================
+# @app.route("/individual")
+# def individual_cupcake():
+#     cupcakes= "jose"
+#     letters = list(cupcakes)
+
+#     cupcake_dictionary = {'cupcake_name': 'strawberry'}
+#     return render_template("individual.html", cupcakes=cupcakes, letters=letters, cupcake_dictionary=cupcake_dictionary)
+
+
 @app.route("/individual")
 def individual_cupcake():
-    return render_template("individual.html")
+    cupcake_list = ['Strawbery Shortcake', 'Red Velvet','Choco Freeze', 'Peanut Butter Swirl'
 
-
-
+    ]
+    return render_template("individual.html",cupcake_list=cupcake_list)
 
     
 
